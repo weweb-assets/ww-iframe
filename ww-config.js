9481dc01171fea640fd20631526bf08434d93ed8
Damien Duro <damien@weweb.io>
2022-10-20 12:49:42 +0200

    Update ww-config.js

--- ww-config.js
+++ ww-config.js
@@ -7,7 +7,7 @@ export default {
             en: 'iFrame',
             fr: 'iFrame',
         },
-        icon: 'fontawesome/regular/window-maximize',
+        icon: 'globa-alt',
     },
     properties: {
         source: {
